42ed0afd8842a3a2f4799ad72c5fb4a37c53903c
Youssef Bouali <53417796+youssefbouali@users.noreply.github.com>
2021-05-21 23:01:24 +0100

    Create embed_api.js

---
+++ embed_api.js
@@ -0,0 +1,3 @@
+function embed_post(id_post, width, height) {
+	document.write('<iframe style="border: 2px solid #b1b1b1; border-radius: 5px;" src="https://www.th3arabe.ml/api/embed.php?post='+id_post+'" height="'+height+'" width="'+width+'" ></iframe>');
+}
