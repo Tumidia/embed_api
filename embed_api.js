e93038ea7b4ea139c564a2792b40bb22d2fe8a94
Youssef Bouali <53417796+youssefbouali@users.noreply.github.com>
2024-03-13 16:45:50 +0000

    Update embed_api.js

--- embed_api.js
+++ embed_api.js
@@ -1,3 +1,3 @@
 function embed_post(id_post, reactions, id_comment, id_reply, width, height) {
-	document.write('<iframe style="border: 2px solid #b1b1b1; border-radius: 5px;" src="https://www.th3arabe.ml/api/embed.php?post='+id_post+'&reactions='+reactions+'&source2='+id_comment+'&source3='+id_reply+'" height="'+height+'" width="'+width+'" ></iframe>');
+	document.write('<iframe style="border: 2px solid #b1b1b1; border-radius: 5px;" src="https://tumidia.wsoum.eu.org/api/embed.php?post='+id_post+'&reactions='+reactions+'&source2='+id_comment+'&source3='+id_reply+'" height="'+height+'" width="'+width+'" ></iframe>');
 }
